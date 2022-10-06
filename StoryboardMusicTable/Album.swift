47383b9e53de5f55ef2fa722fa34dc93f216e9cf
Abby Dominguez <abbytorade@icloud.com>
2022-10-06 19:20:03 +0200

    ViewController method

---
+++ StoryboardMusicTable/Album.swift
@@ -0,0 +1,31 @@
+//
+//  Album.swift
+//  StoryboardMusicTable
+//
+//  Created by Abby Dominguez on 6/10/22.
+//
+
+import Foundation
+class Album: AlbumProtocol {
+    init(name: String, artist: String, imageURL: String){
+        albumName = name
+        albumArtist = artist
+        albumImage = imageURL
+    }
+    var albumName: String
+    var albumImage: String
+    var albumArtist: String
+    
+    func getTitle() -> String {
+        return self.albumName
+    }
+    func getImg() -> String {
+        return self.albumImage
+    }
+    
+    func getArtist() -> String {
+        return self.albumArtist
+    }
+    
+    
+}
---
+++ StoryboardMusicTable/MusicCell.swift
@@ -0,0 +1,15 @@
+//
+//  MusicCell.swift
+//  StoryboardMusicTable
+//
+//  Created by Abby Dominguez on 6/10/22.
+//
+
+import UIKit
+class MusicCell: UITableViewCell{
+    
+    @IBOutlet weak var artistOutlet: UILabel!
+    @IBOutlet weak var titleOutlet: UILabel!
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
+}
