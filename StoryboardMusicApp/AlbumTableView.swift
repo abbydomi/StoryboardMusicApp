3f09e975544b526778080495952b73e828bd8c28
Abby Dominguez <abbytorade@icloud.com>
2022-10-18 16:17:41 +0200

    added pods

---
+++ StoryboardMusicApp/AlbumTableView.swift
@@ -0,0 +1,16 @@
+//
+//  AlbumTableView.swift
+//  StoryboardMusicApp
+//
+//  Created by Abby Dominguez on 17/10/22.
+//
+
+import UIKit
+class AlbumTableView: UITableView {
+
+}
+
+class AlbumDetailCell: UITableViewCell {
+    @IBOutlet weak var trackNo: UILabel!
+    @IBOutlet weak var trackName: UILabel!
+}
